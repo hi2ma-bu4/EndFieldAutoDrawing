c59f7992f40364d98da248d8ae000528e82ad359
hi2ma-bu4 <hi2ma.bu4.bot@gmail.com>
2026-03-30 14:18:15 +0900

    🎉 init

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+/*!
+ * index 0.0.1
+ * Copyright 2026 hi2ma-bu4
+ * Licensed under the Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+//# sourceMappingURL=index.js.map
